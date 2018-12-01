e6b8d16d45b91364696ccd0390e5d8fbf143e2f6
Sam Mussell <smussell@gmail.com>
2018-11-30 23:23:33 -0800

    Update day2-b.clj

--- day2-b.clj
+++ day2-b.clj
@@ -1,8 +1,8 @@
-(def m (cycle [-6, +3, +8, +5, -6]))
+(def input (cycle [-6, +3, +8, +5, -6]))
 
-(loop [resSet #{} running 0 list m]
-  (if (contains? resSet running)
+(loop [results #{} running 0 list input]
+  (if (contains? results running)
     running
-    (recur (conj resSet running)
+    (recur (conj results running)
            (+ running (first list))
            (next list))))
