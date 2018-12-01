d87d868826e833eac86d0800e2a667681ec01c19
Sam Mussell <smussell@gmail.com>
2018-11-30 22:56:30 -0800

    Update day2-b.clj

--- day2-b.clj
+++ day2-b.clj
@@ -1,8 +1,8 @@
 (def m (cycle [-6, +3, +8, +5, -6]))
 
-(loop [resSet #{} running 0 ind 0]
+(loop [resSet #{} running 0 list m]
   (if (contains? resSet running)
     running
     (recur (conj resSet running)
-           (+ running (nth m ind))
-           (inc ind))))
+           (+ running (first list))
+           (next list))))
