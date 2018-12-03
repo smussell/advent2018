e474d6b0cb0a84497e94c238cd29c042a20ff32e
Sam Mussell <smussell@apple.com>
2018-12-03 00:25:37 -0800

    start day 3

---
+++ day3-a.clj
@@ -0,0 +1,21 @@
+(defn nn [n] #(nth %1 n))
+
+(def pattern #"#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)")
+
+(def codes ["#1 @ 1,3: 4x4"
+            "#2 @ 3,1: 4x4"
+            "#3 @ 5,5: 2x2"])
+
+
+
+(defn extract [s] (drop 2 (re-matches pattern s)))
+(def parsed 
+  (map #(map read-string (extract %1)) codes))
+
+(def outerBounds [
+                  (apply min (map (nn 0) parsed))
+                  (apply min (map (nn 1) parsed))
+                  (apply max (map #(+ (nth %1 0) (nth %1 2)) parsed))
+                  (apply max (map #(+ (nth %1 1) (nth %1 3)) parsed))
+                  ])
+
