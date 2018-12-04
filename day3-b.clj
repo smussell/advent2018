456cea06332bb2105a243e13c8fa501e8f275380
Sam Mussell <smussell@apple.com>
2018-12-03 21:49:55 -0800

    day 3

--- day3-b.clj
+++ day3-b.clj
@@ -6,23 +6,24 @@
             "#2 @ 3,1: 4x4"
             "#3 @ 5,5: 2x2"])
 
-(defn extract [s] (drop 2 (re-matches pattern s)))
-(def dim-keys [:x :y :width :height])
+(defn extract [s] (drop 1 (re-matches pattern s)))
+(def dim-keys [:i :x :y :width :height])
 
-(def parsed 
-  (map #(->> (extract %1) 
-             (map read-string) 
-             (zipmap dim-keys)) 
+(def parsed
+  (map #(->> (extract %1)
+             (map read-string)
+             (zipmap dim-keys))
        codes))
 
 (defn get-squares [{:keys [x y width height]}]
   (for [i (range width) j (range height)]
     [(+ x i) (+ y j)]))
 
-(defn update-grid [grid box] 
+(defn update-grid [grid box]
   (->>
    (get-squares box)
    (reduce #(assoc %1 %2 (inc (get %1 %2 0))) grid)))
 
 (def grid (reduce update-grid {} parsed))
-(count (filter #(> (val %1) 1) grid))
+
+(filter (fn [box] (every? #(= 1 (get grid %1)) (get-squares box))) parsed)
